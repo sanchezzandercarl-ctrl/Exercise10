afaa83dd9210aa3a81b77b20440169af6e118a14
sanchezzandercarl-ctrl <sanchezzandercarl@mail.com>
2025-11-07 18:33:24 +0800

    applying code to form and to button add

--- Exercise10/Form1.vb
+++ Exercise10/Form1.vb
@@ -1,3 +1,19 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Linq
+
+Public Class Form1
+    Dim numbers As New List(Of Integer)
+    Dim filePath As String = "numbers.txt"
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim num As Integer
+        If Integer.TryParse(txtNumber.Text, num) Then
+            numbers.Add(num)
+            txtNumber.Clear()
+            txtNumber.Focus()
+            MessageBox.Show("Number added successfully!")
+        Else
+            MessageBox.Show("Please enter a valid number.")
+        End If
+    End Sub
 
 End Class
