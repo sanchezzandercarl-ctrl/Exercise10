cffe6bf0be44fc6c8f493a80070b95cf47a82380
sanchezzandercarl-ctrl <sanchezzandercarl@mail.com>
2025-11-07 18:16:26 +0800

    Done labeling

--- Exercise10/Form1.Designer.vb
+++ Exercise10/Form1.Designer.vb
@@ -22,67 +22,86 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lblInstruction = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnReadSort = New System.Windows.Forms.Button()
+        Me.lstNumbers = New System.Windows.Forms.ListBox()
+        Me.lblResult = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblInstruction
         '
-        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Label1.Location = New System.Drawing.Point(12, 136)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(255, 118)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.lblInstruction.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblInstruction.Location = New System.Drawing.Point(12, 136)
+        Me.lblInstruction.Name = "lblInstruction"
+        Me.lblInstruction.Size = New System.Drawing.Size(255, 118)
+        Me.lblInstruction.TabIndex = 0
         '
-        'TextBox1
+        'txtNumber
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(255, 121)
-        Me.TextBox1.TabIndex = 1
+        Me.txtNumber.Location = New System.Drawing.Point(12, 12)
+        Me.txtNumber.Multiline = True
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(255, 121)
+        Me.txtNumber.TabIndex = 1
         '
-        'Button1
+        'btnAdd
         '
-        Me.Button1.Location = New System.Drawing.Point(295, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(136, 62)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnAdd.Location = New System.Drawing.Point(295, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(136, 62)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnSave
         '
-        Me.Button2.Location = New System.Drawing.Point(295, 80)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(136, 62)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnSave.Location = New System.Drawing.Point(295, 80)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(136, 62)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "SAVE"
+        Me.btnSave.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnReadSort
         '
-        Me.Button3.Location = New System.Drawing.Point(295, 148)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(136, 62)
-        Me.Button3.TabIndex = 4
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnReadSort.Location = New System.Drawing.Point(295, 148)
+        Me.btnReadSort.Name = "btnReadSort"
+        Me.btnReadSort.Size = New System.Drawing.Size(136, 62)
+        Me.btnReadSort.TabIndex = 4
+        Me.btnReadSort.Text = "SORT"
+        Me.btnReadSort.UseVisualStyleBackColor = True
+        '
+        'lstNumbers
+        '
+        Me.lstNumbers.FormattingEnabled = True
+        Me.lstNumbers.Location = New System.Drawing.Point(12, 302)
+        Me.lstNumbers.Name = "lstNumbers"
+        Me.lstNumbers.Size = New System.Drawing.Size(255, 121)
+        Me.lstNumbers.TabIndex = 5
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblResult.Location = New System.Drawing.Point(277, 302)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(255, 117)
+        Me.lblResult.TabIndex = 6
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(544, 450)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lstNumbers)
+        Me.Controls.Add(Me.btnReadSort)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.lblInstruction)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -90,9 +109,11 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents lblInstruction As Label
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnReadSort As Button
+    Friend WithEvents lstNumbers As ListBox
+    Friend WithEvents lblResult As Label
 End Class
